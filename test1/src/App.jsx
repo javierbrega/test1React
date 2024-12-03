7e172bac7295f2e46a4feaaa77c52af5d83e77be
Javier Brega <javierbrega@gmail.com>
2024-12-03 15:24:13 -0300

    agregando componentes al header y footer

--- test1/src/App.jsx
+++ test1/src/App.jsx
@@ -1,8 +1,18 @@
+// src/App.jsx
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+
 function App() {
   return (
     <div>
+      <Header />
+      <main>
       <h1>!Hola, Mundo!</h1>
       <p>Tu configuracion de React con Vite está lista para funcionar</p>
+      </main>
+      <footer />
     </div>
   );
 }
---
+++ test1/src/Footer.jsx
@@ -0,0 +1,12 @@
+// src/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 Javier Brega. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
