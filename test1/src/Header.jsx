5ea282101d80e1509a576adc27e45b5e46ba041a
Javier Brega <javierbrega@gmail.com>
2024-12-04 15:43:58 -0300

    menu hamburguesa responsivo

--- test1/src/Header.jsx
+++ test1/src/Header.jsx
@@ -1,20 +1,32 @@
-// src/ Header.jsx
+// src/Header.jsx
 
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-function Header(){
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="#home">Inicio</a></li>
-                    <li><a href="#about">Acerca de</a></li>
-                    <li><a href="#services">Servicios</a></li>
-                    <li><a href="#contact">Contacto</a></li>
-                </ul>
-            </nav>
-        </header>    
-    );
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header>
+      <nav>
+        <div className="menu-icon" onClick={toggleMenu}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={`nav-menu ${isOpen ? "open" : ""}`}>
+          <li><a href="#home">Inicio</a></li>
+          <li><a href="#Moviliario">Moviliario</a></li>
+          <li><a href="#Construcción">Construcción</a></li>
+          <li><a href="#Pedidos">Pedidos OnLine</a></li>
+          <li><a href="#Sobre Nosotros">Sobre Nosotros</a></li>
+          <li><a href="#Contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
