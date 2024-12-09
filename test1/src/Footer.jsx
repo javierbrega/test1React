405730c0d8d059891e29366d2bfa0502e2192048
Javier Brega <javierbrega@gmail.com>
2024-12-09 00:03:00 -0300

    agragando elementos al footer

--- test1/src/Footer.jsx
+++ test1/src/Footer.jsx
@@ -4,8 +4,23 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>© 2024 Javier Brega. Todos los derechos reservados.</p>
       
+      <div className='datos'>
+        <p>
+          <strong>San Rafael - Mendoza</strong>
+          <br />
+          "Av. Alverdi 1245"
+          <br />
+          <strong>Teléfono:</strong> 0260-4421356
+          <br />
+          <strong>Email:</strong>          
+        </p>
+
+      </div>
+      
+      
+    
+      <p>© 2024 Javier Brega. Todos los derechos reservados.</p>
     </footer>
   );
 }
