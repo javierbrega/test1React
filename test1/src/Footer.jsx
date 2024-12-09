02dac1757b8c0f2773f9da3c2bc3d669c5268056
Javier Brega <javierbrega@gmail.com>
2024-12-09 00:38:50 -0300

    sigo avanzando con el footer agrego 3 columnas para datos

--- test1/src/Footer.jsx
+++ test1/src/Footer.jsx
@@ -3,20 +3,24 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    
       
-      <div className='datos'>
-        <p>
-          <strong>San Rafael - Mendoza</strong>
-          <br />
-          "Av. Alverdi 1245"
-          <br />
-          <strong>Teléfono:</strong> 0260-4421356
-          <br />
-          <strong>Email:</strong>          
-        </p>
+      
+<footer>
+  <div className="footer-column">
+    <h3>Columna 1</h3>
+    <p>Contenido de la columna 1</p>
+  </div>
+  <div className="footer-column">
+    <h3>Columna 2</h3>
+    <p>Contenido de la columna 2</p>
+  </div>
+  <div className="footer-column">
+    <h3>Columna 3</h3>
+    <p>Contenido de la columna 3</p>
+  </div>
 
-      </div>
+
       
       
     
