258adf7b1af79aec91e4af3f4f8c991951dc86a3
Javier Brega <javierbrega@gmail.com>
2024-12-10 18:47:14 -0300

    sigo trabajando en el footer

--- test1/src/Footer.jsx
+++ test1/src/Footer.jsx
@@ -1,30 +1,22 @@
 // src/Footer.jsx
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
-    
-      
-      
-<footer>
-  <div className="footer-column">
-    <h3>Columna 1</h3>
-    <p>Contenido de la columna 1</p>
-  </div>
-  <div className="footer-column">
-    <h3>Columna 2</h3>
-    <p>Contenido de la columna 2</p>
-  </div>
-  <div className="footer-column">
-    <h3>Columna 3</h3>
-    <p>Contenido de la columna 3</p>
-  </div>
-
-
-      
-      
-    
-      <p>© 2024 Javier Brega. Todos los derechos reservados.</p>
+    <footer>
+      <div className="footer-column">
+        <h3>Columna 1</h3>
+        <p>Contenido de la columna 1</p>
+      </div>
+      <div className="footer-column">
+        <h3>Columna 2</h3>
+        <p>Contenido de la columna 2</p>
+      </div>
+      <div className="footer-column">
+        <h3>Columna 3</h3>
+        <p>Contenido de la columna 3</p>
+      </div>
+      <p className="derechos">© 2024 Javier Brega. Todos los derechos reservados.</p>
     </footer>
   );
 }
