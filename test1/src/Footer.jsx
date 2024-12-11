80a6c2e8175213c8f7fc5e270fa45402a462313a
Javier Brega <javierbrega@gmail.com>
2024-12-11 01:01:27 -0300

    sigo trabajando con el footer ahora con flexbox

--- test1/src/Footer.jsx
+++ test1/src/Footer.jsx
@@ -4,19 +4,73 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <div className="footer-column">
-        <h3>Columna 1</h3>
-        <p>Contenido de la columna 1</p>
-      </div>
-      <div className="footer-column">
-        <h3>Columna 2</h3>
-        <p>Contenido de la columna 2</p>
-      </div>
-      <div className="footer-column">
-        <h3>Columna 3</h3>
-        <p>Contenido de la columna 3</p>
-      </div>
-      <p className="derechos">© 2024 Javier Brega. Todos los derechos reservados.</p>
+      <section className="footer-flex-container">
+        <div className="footer-column">
+          <h3>
+            <strong>Maderera Lopez</strong>
+          </h3>
+          <p>San Rafael - Mendoza - Argentina</p>
+          <p>Tel: 0260 440-4918</p>
+          <p>Dir: Av. Juan B. Alberdi 2100</p>
+        </div>
+        <div className="footer-column">
+          <h3>Accesos Rápidos</h3>
+          <ul>
+            <li>
+              <a href="#home">Inicio</a>
+            </li>
+            <li>
+              <a href="#productos">Productos</a>
+            </li>
+            <li>
+              <a href="#contacto">Construcción</a>
+            </li>
+            <li>
+              <a href="#home">Pedidos OnLine</a>
+            </li>
+            <li>
+              <a href="#productos">Sobre Nosotros </a>
+            </li>
+            <li>
+              <a href="#contacto">Contacto</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h3>
+            <strong>Redes Sociales</strong>
+          </h3>{" "}
+          <a href="https://www.instagram.com/madereralopezsr/" target="_blank">
+            {" "}
+            <img
+              src="src\assets\icons\instagram.svg"
+              alt="instagram logo"
+              width={30}
+            />{" "}
+          </a>
+          <a href="https://www.facebook.com/madereralopezsr/" target="_blank">
+            {" "}
+            <img
+              src="src\assets\icons\facebook.svg"
+              alt="facebook logo"
+              width={30}
+            />{" "}
+          </a>
+          <a href="#linkedin" target="_blank">
+            {" "}
+            <img
+              src="src\assets\icons\linkedin.svg"
+              alt="linkedin logo"
+              width={30}
+            />{" "}
+          </a>
+        </div>
+      </section>
+      <hr />
+      <section>
+        <p>© 2021 Maderera Lopez - Todos los derechos reservados</p>
+      </section>
+
     </footer>
   );
 }
